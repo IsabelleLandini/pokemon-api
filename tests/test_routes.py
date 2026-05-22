de8114fa03f56757fb6e135901e0e2cfdbe22a50
Isabelle <isa_landini@hotmail.com>
2026-05-22 12:57:31 -0300

    test: adiciona testes de paginação da API

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,4 +12,16 @@ def test_get_pokemons():
 
     assert 'data' in data
     assert 'pagination' in data
-    
+
+def test_get_pokemons_with_pagination():
+    response = client.get(
+        '/pokemons?limit=5&offset=0'
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert len(data['data']) == 5
+    assert data['pagination']['limit'] == 5
+    assert data['pagination']['offset'] == 0
