f6c823a4c998d86c9e40e562f0695fcbd9a9737f
Isabelle <isa_landini@hotmail.com>
2026-05-25 15:52:53 -0300

    feat: adiciona documentação swagger

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ from app.routes.pokemon_router import router as pokemon_router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title='Pokemon API',
+    version='1.0.0'
+)
 
 app.include_router(pokemon_router)
 
