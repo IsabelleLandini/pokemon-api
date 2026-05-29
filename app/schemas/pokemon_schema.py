1e9ab48e2c98f04ea709f417c1ff525699cc611c
Isabelle <isa_landini@hotmail.com>
2026-05-29 12:12:14 -0300

    refactor: prepara estrutura de persistência no banco

---
+++ app/schemas/pokemon_schema.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+class PokemonSprites(BaseModel):
+    front_default: str | None
+    back_default: str | None
+
+class PokemonResponse(BaseModel):
+    id: int
+    name: str
+    height: int
+    weight: int
+    types: str
+    sprites: PokemonSprites
+    stats: dict[str, int] | None = None
+
+class PaginationResponse(BaseModel):
+    total: int 
+    limit: int
+    offset: int
+    next: str | None
+    previous: str |  None
+
+class PokemonListResponse(BaseModel):
+    data: list[PokemonResponse]
+    pagination: PaginationResponse
+
+class PokemonCreate(BaseModel):
+    pokemon_id: int
+    name: str 
+    height: int
+    weight: int 
+    types: str 
+    front_sprite: str
+    back_sprite: str
